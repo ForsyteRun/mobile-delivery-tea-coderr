556fe8c7a02948bac9df9576b01b646a4ca59f57
Forsyte <forsyterun@gmail.com>
2025-07-26 07:40:35 +0200

    feat: prisma & postgress

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prismaService: PrismaService) { }
+  async getAllUsers() {
+    return this.prismaService.user.findMany()
   }
 }
