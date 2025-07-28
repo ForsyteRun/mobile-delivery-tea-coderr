53b4230863aaff3ef10b1d0f74c6c8ecb0f5c320
Forsyte <forsyterun@gmail.com>
2025-07-28 09:03:56 +0200

    feat: decorators

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Auth } from './decorators/auth.decorator';
 import { AuthRequest } from './dto/auth.dto';
 import { RefreshTokensRequest } from './dto/refresh-tokens.dto';
 
@@ -14,15 +14,16 @@ export class AuthController {
     return await this.authService.register(dto);
   }
 
-  @UseGuards(AuthGuard('jwt'))
   @HttpCode(HttpStatus.OK)
   @Post('login')
+  @Auth()
   async login(@Body() dto: AuthRequest) {
     return await this.authService.login(dto);
   }
 
   @HttpCode(HttpStatus.OK)
   @Post('refresh')
+  @Auth()
   async refreshTokens(@Body() dto: RefreshTokensRequest) {
     return await this.authService.refreshTokens(dto);
   }
