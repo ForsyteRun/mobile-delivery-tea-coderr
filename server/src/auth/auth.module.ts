3b3c041bc2085a85be53debcd94dc14d4e6e35ed
Forsyte <forsyterun@gmail.com>
2025-07-28 08:21:19 +0200

    feat: jwt register

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { gerJwtConfig } from 'src/config/jwt.config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
+  imports: [ConfigModule, JwtModule.registerAsync({ imports: [ConfigModule], inject: [ConfigService], useFactory: gerJwtConfig })],
 })
 export class AuthModule { }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,11 +1,15 @@
+import { faker } from '@faker-js/faker';
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { hash } from 'argon2';
+import { JwtService } from "@nestjs/jwt";
+import { User } from '@prisma/client';
+import { hash, verify } from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthRequest } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService) { }
+  constructor(private readonly prismaService: PrismaService,
+    private readonly jwtService: JwtService) { }
 
   async register(dto: AuthRequest) {
     const { email, password } = dto
@@ -18,6 +22,63 @@ export class AuthService {
 
     const hashPassword = await hash(password);
 
-    return await this.prismaService.user.create({ data: { email, password: hashPassword, } });
+    const userCreated = await this.prismaService.user.create(
+      {
+        data:
+        {
+          email,
+          password: hashPassword,
+          avatarPath: faker.image.avatar(),
+          name: faker.person.firstName(),
+          phone: faker.phone.number()
+        }
+      });
+
+    const tokens = await this.generateTokens(userCreated.id);
+
+    return {
+      user: this.getUserFields(userCreated),
+      ...tokens
+    }
+  }
+
+  private async generateTokens(id: string) {
+    const data = { id };
+
+    const accessToken = this.jwtService.sign(data, {
+      expiresIn: '1d',
+    });
+
+    const refreshToken = this.jwtService.sign(data, {
+      expiresIn: '7d',
+    });
+
+    return {
+      accessToken,
+      refreshToken
+    }
+  }
+
+  private getUserFields(user: User) {
+    return {
+      id: user.id,
+      email: user.email,
+    };
+  }
+
+  async validateUser(email: string, password: string) {
+    const user = await this.prismaService.user.findUnique({ where: { email } });
+
+    if (!user) {
+      throw new NotFoundException('User does not exist');
+    }
+
+    const isPasswordValid = await verify(user.password, password);
+
+    if (!isPasswordValid) {
+      throw new NotFoundException('User does not exist');
+    }
+
+    return user;
   }
 }
